10bbad0a360dad0e55fdf381cd0e448d041655d3
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-10-14 14:19:16 +0200

    ADDED image from file in Photo, ADDED call to action in Photo/Details

--- net-il-mio-fotoalbum/Models/Db/Photo.cs
+++ net-il-mio-fotoalbum/Models/Db/Photo.cs
@@ -5,6 +5,7 @@ namespace net_il_mio_fotoalbum.Models.Db
     [Table("photo")]
     public class Photo
     {
+
         [Column("id")]
         public int Id { get; set; }
         
@@ -16,6 +17,8 @@ namespace net_il_mio_fotoalbum.Models.Db
         
         [Column("image")]
         public byte[]? ImageFile { get; set; }
+        public string? ImageUrl => ImageFile is null ? "https://www.technol.si/wp-content/uploads/2018/11/default-image1.jpg" : $"data:image/png;base64,{Convert.ToBase64String(ImageFile)}";
+
 
         [Column("visible")]
         public bool Visible {  get; set; }
